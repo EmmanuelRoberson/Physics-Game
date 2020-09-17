19bd12adef14435308322b9319ed85dae0d2a8bc
emmanuel <emmanuel.roberson3@gmail.com>
2020-09-17 12:57:03 -0500

    Added Navigations agents and mesh
    Added nav agents to the scene. Added debug scripts to test the nav agent destination assignment.

---
+++ PhysicsGameProject/Assets/Scripts/AI/AI Navigation/Debug/NavMeshAgentBehaviourDebugger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavMeshAgentBehaviourDebugger : MonoBehaviour
+{
+    public Transform[] transformArray;
+
+    private NavMeshAgentBehaviour _navMeshAgentBehaviour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgentBehaviour = GetComponent<NavMeshAgentBehaviour>();
+        _navMeshAgentBehaviour.AddDestinationsToQueue(transformArray);
+        _navMeshAgentBehaviour.QueNextDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other1)
+    {
+
+        TaskSourceBehaviourDebugger taskSource = other1.gameObject.GetComponent<TaskSourceBehaviourDebugger>();
+
+        if (taskSource != null)
+        {
+            
+            _navMeshAgentBehaviour.QueNextDestination();
+        }
+    }
+}
---
+++ PhysicsGameProject/Assets/Scripts/AI/AI Navigation/NavMeshAgentBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class NavMeshAgentBehaviour : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+    private Queue<Transform> _destinationQueue;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _destinationQueue = new Queue<Transform>();
+    }
+
+    //Enqueue more Vector3s to _destinationQueue
+    public void AddDestinationsToQueue(params Transform[] destinations)
+    {
+        for (int i = 0; i < destinations.Length; i++)
+        {
+            _destinationQueue.Enqueue(destinations[i]);
+        }
+    }
+
+    public void QueNextDestination()
+    {
+        _navMeshAgent.SetDestination(_destinationQueue.Dequeue().position);
+    } 
+}
